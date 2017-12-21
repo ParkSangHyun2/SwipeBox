ee879aebd7b7576316bf1f1e9b60b6c50cdb4850
ParkSangHyun2 <pus5684@gmail.com>
2017-12-21 19:25:48 +0900

    update swipebox

---
+++ sanghyun/target/test-classes/sql/share.sql
@@ -0,0 +1,8 @@
+CREATE TABLE sharefile (
+	fileUid		VARCHAR(30)  NOT NULL,
+	userId 		VARCHAR(20)  NOT NULL,
+	fileName    	VARCHAR(20)  NOT NULL,
+	fileSize   	VARCHAR(30)  NOT NULL,
+	fileType   	VARCHAR(20)  NOT NULL,
+	regDate 		DATETIME
+);
